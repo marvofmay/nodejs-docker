0e4be6e7971a0d68fedd592560aed47412f1bf0f
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-05-03 11:03:53 +0200

    Add sweetalert2 and use for confirm manufacturer delete

--- src/views/manufacturers/js/info.js
+++ src/views/manufacturers/js/info.js
@@ -2,18 +2,40 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnDeleteManufacturers = document.querySelectorAll('a.btn-delete-manufacturer');
     btnDeleteManufacturers.forEach(btnDeleteManufacturer => {
         btnDeleteManufacturer.addEventListener('click', (e) => {
-            const shouldDelete = confirm('Czy na pewno chcesz usunąć tego producenta?');
-            if (shouldDelete) {
-                const endpoint = `/manufacturers/${btnDeleteManufacturer.dataset.doc}`;
-                fetch(endpoint, {
-                    method: 'DELETE',
-                })
-                    .then(response => response.json())
-                    .then(data => window.location.href = data.redirect)
-                    .catch(err => console.log(err));
-            } else {
-                e.preventDefault();
-            }
+
+            Swal.fire({
+                title: "Do you want to delete this manufacturer?",
+                showCancelButton: true,
+                confirmButtonText: "Delete",
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    const endpoint = `/manufacturers/${btnDeleteManufacturer.dataset.doc}`;
+                    fetch(endpoint, {
+                        method: 'DELETE',
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            const element = document.querySelector('#info-manufacturer-content');
+                            if (element) {
+                                element.remove();
+                                Toastify({
+                                    text: data.actionResult.message,
+                                    duration: 2500,
+                                    newWindow: true,
+                                    gravity: 'top',
+                                    position: 'right',
+                                    style: {
+                                        background: "linear-gradient(to right top, #8ec021, #82be29, #77bd31, #6abb37, #5eb93e)",
+                                    }
+                                }).showToast();
+                            }
+                        })
+                        .catch(err => console.log(err));
+                } else if (result.isDenied) {
+                    e.preventDefault();
+                }
+            });
         });
     });
 });
