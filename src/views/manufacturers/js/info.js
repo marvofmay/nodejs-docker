b98e523524f7b35e0033cc96f9fe55cb099ea7c3
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-05-03 11:04:26 +0200

    Add sweetalert2 and use for confirm manufacturer delete

--- src/views/manufacturers/js/info.js
+++ src/views/manufacturers/js/info.js
@@ -2,13 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnDeleteManufacturers = document.querySelectorAll('a.btn-delete-manufacturer');
     btnDeleteManufacturers.forEach(btnDeleteManufacturer => {
         btnDeleteManufacturer.addEventListener('click', (e) => {
-
             Swal.fire({
                 title: "Do you want to delete this manufacturer?",
                 showCancelButton: true,
                 confirmButtonText: "Delete",
             }).then((result) => {
-                /* Read more about isConfirmed, isDenied below */
                 if (result.isConfirmed) {
                     const endpoint = `/manufacturers/${btnDeleteManufacturer.dataset.doc}`;
                     fetch(endpoint, {
@@ -21,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 element.remove();
                                 Toastify({
                                     text: data.actionResult.message,
-                                    duration: 2500,
+                                    duration: 3000,
                                     newWindow: true,
                                     gravity: 'top',
                                     position: 'right',
