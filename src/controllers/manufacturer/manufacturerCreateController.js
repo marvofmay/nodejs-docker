f68a1aece104404b9c5213f6cf8a78bc3fe9f39b
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-05-07 20:19:11 +0200

    Refactor  manufacturer create controller

--- src/controllers/manufacturer/manufacturerCreateController.js
+++ src/controllers/manufacturer/manufacturerCreateController.js
@@ -1,21 +1,25 @@
 const manufacturerRepository = require("../../repositories/manufacturer/repository");
 const CreateManufacturerDTO = require("../../dto/manufacturer/CreateManufacturerDTO");
 const manufacturerCreate = async (req, res) => {
+    try {
+        const manufacturers = await manufacturerRepository.getAllManufacturersForSelectOptions();
+        const manufacturersForSelect = manufacturers.map(manufacturer => ({
+            _id: manufacturer._id,
+            name: manufacturer.name
+        }));
 
-    const manufacturers = await manufacturerRepository.getAllManufacturersForSelectOptions();
-    const manufacturersForSelect = manufacturers.map(manufacturer => ({
-        _id: manufacturer._id,
-        name: manufacturer.name
-    }));
+        res.render('manufacturers/create', {
+            title: 'Manufacturers',
+            action: 'Create a new manufacturer',
+            manufacturer: {},
+            manufacturers: manufacturersForSelect,
+            errors: [],
+            actionResult: {}
+        });
+    } catch(error) {
+        res.render('error/404error', {title: '404 error', message: error.message});
+    }
 
-    res.render('manufacturers/create', {
-        title: 'Manufacturers',
-        action: 'Create a new manufacturer',
-        manufacturer: {},
-        manufacturers: manufacturersForSelect,
-        errors: [],
-        actionResult: {}
-    });
 }
 
 module.exports = {
