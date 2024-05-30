a38e61f41ba47034737622b41d526e2ade3f2f96
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-05-30 13:53:25 +0200

    Change some code JS in create / edit manufacturer

--- src/views/manufacturers/js/create.js
+++ src/views/manufacturers/js/create.js
@@ -1,6 +1,6 @@
 $(() => {
     $('#parentManufacturer').select2({
-        placeholder: 'Select a parent manufacturer',
+        placeholder: "Select a parent manufacturer",
     });
     $('#country').select2({
         placeholder: 'Select country',
@@ -8,83 +8,83 @@ $(() => {
     $('#city').select2({
         placeholder: 'Select city',
     });
-})
 
-const fetchCountryData = async () => {
-    const endpoint = 'https://countriesnow.space/api/v0.1/countries/iso';
+    const fetchCountryData = async () => {
+        const endpoint = 'https://countriesnow.space/api/v0.1/countries/iso';
 
-    try {
-        const response = await fetch(endpoint);
-        if (! response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+        try {
+            const response = await fetch(endpoint);
+            if (! response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result = await response.json();
+            const data = result.data;
+
+            return data.map(country => country.name);
+        } catch (error) {
+            console.error('Błąd podczas pobierania danych:', error);
         }
-        const result = await response.json();
-        const data = result.data;
-
-        return data.map(country => country.name);
-    } catch (error) {
-        console.error('Błąd podczas pobierania danych:', error);
-    }
-};
-
-const populateSelect = async (selectElementId, country = '') => {
-    const selectElement = document.getElementById(selectElementId);
-    let items;
-    if (selectElementId === 'country') {
-        items = await fetchCountryData();
-    }
-    if (selectElementId === 'city') {
-        items = await fetchCitiesDataForCountry(country);
-        selectElement.innerHTML = '';
-    }
-    items.forEach((item) => {
-        const option = document.createElement('option');
-        option.value = item;
-        option.textContent = item;
-        selectElement.appendChild(option);
-    });
-    if (selectElementId === 'city') {;
-        selectElement.removeAttribute('disabled');
-    }
-};
-
-populateSelect('country');
-
-const fetchCitiesDataForCountry = async (country) => {
-    const endpoint = 'https://countriesnow.space/api/v0.1/countries/cities';
-    const data = {
-        country: country
     };
 
-    try {
-        const response = await fetch(endpoint, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
+    const populateSelect = async (selectElementId, country = '') => {
+        const selectElement = $(`#${selectElementId}`);
+        let items;
+        if (selectElementId === 'country') {
+            items = await fetchCountryData();
+        }
+        if (selectElementId === 'city') {
+            items = await fetchCitiesDataForCountry(country);
+            selectElement.empty();
+        }
+        const option = $('<option></option>').val('').text('');
+        $(selectElement).append(option);
+        items.forEach((item) => {
+            const option = $('<option></option>').val(item).text(item);
+            $(selectElement).append(option);
         });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+        if (selectElementId === 'city') {
+            selectElement.removeAttr('disabled');
         }
+    };
 
-        const result = await response.json();
+    populateSelect('country');
 
-        return result.data;
-    } catch (error) {
-        console.error('Błąd podczas pobierania danych:', error);
+    const fetchCitiesDataForCountry = async (country) => {
+        const endpoint = 'https://countriesnow.space/api/v0.1/countries/cities';
+        const data = {
+            country: country
+        };
 
-        return [];
-    }
-};
+        try {
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
 
-$(() => {
-    $('#country').on('select2:select', function (e) {
-        const selectedValue = $(this).val();
-        if (selectedValue !== '') {
-            $('#city').attr('disabled', 'disabled');
-            populateSelect('city', selectedValue);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+
+            return result.data;
+        } catch (error) {
+            console.error('Błąd podczas pobierania danych:', error);
+
+            return [];
         }
+    };
+
+    $(() => {
+        $('#country').on('change', function (e) {
+            const selectedCountry = $(this).val();
+            if (selectedCountry !== '') {
+                $('#city').attr('disabled', 'disabled');
+                populateSelect('city', selectedCountry);
+            }
+        });
     });
-});
+})
--- src/views/manufacturers/js/edit.js
+++ src/views/manufacturers/js/edit.js
@@ -8,4 +8,85 @@ $(() => {
     $('#city').select2({
         placeholder: 'Select city',
     });
+
+    const fetchCountryData = async () => {
+        const endpoint = 'https://countriesnow.space/api/v0.1/countries/iso';
+
+        try {
+            const response = await fetch(endpoint);
+            if (! response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result = await response.json();
+            const data = result.data;
+
+            return data.map(country => country.name);
+        } catch (error) {
+            console.error('Błąd podczas pobierania danych:', error);
+        }
+    };
+
+    const populateSelect = async (selectElementId, country = '') => {
+        const selectElement = $(`#${selectElementId}`);
+        let items;
+        if (selectElementId === 'country') {
+            items = await fetchCountryData();
+        }
+        if (selectElementId === 'city') {
+            items = await fetchCitiesDataForCountry(country);
+            selectElement.empty();
+        }
+        items.forEach((item) => {
+            const option = $('<option></option>').val(item).text(item);
+            $(selectElement).append(option);
+        });
+        if (selectElementId === 'country') {
+            selectElement.val(selectElement.attr('data-current-country')).trigger('change');
+        }
+        if (selectElementId === 'city') {
+            selectElement.removeAttr('disabled');
+            selectElement.val(selectElement.attr('data-current-city'));
+        }
+    };
+
+    populateSelect('country');
+
+    const fetchCitiesDataForCountry = async (country) => {
+        const endpoint = 'https://countriesnow.space/api/v0.1/countries/cities';
+        const data = {
+            country: country
+        };
+
+        try {
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+
+            return result.data;
+        } catch (error) {
+            console.error('Błąd podczas pobierania danych:', error);
+
+            return [];
+        }
+    };
+
+    $(() => {
+        $('#country').on('change', function (e) {
+            const selectedCountry = $(this).val();
+            if (selectedCountry !== '') {
+                $('#city').attr('disabled', 'disabled');
+                populateSelect('city', selectedCountry);
+            }
+        });
+    });
 })
