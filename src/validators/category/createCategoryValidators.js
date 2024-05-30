a49ff767e0915186cb2a2ef54d6c0f4048b87569
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-05-30 20:59:20 +0200

    Add some CSS style for view manufacturer edit and create

--- src/validators/category/createCategoryValidators.js
+++ src/validators/category/createCategoryValidators.js
@@ -4,7 +4,7 @@ const CategoryRepository = require('../../repositories/category/repository');
 const createCategoryValidator = [
     body('name')
         .trim()
-        .notEmpty().withMessage('Name is required.')
+        .notEmpty().withMessage('Name is required')
         .isLength({ min: 3 }).withMessage('Name must be at least 3 characters long')
         .custom(async (name) => {
             const category = await CategoryRepository.getCategoryByName(name);
