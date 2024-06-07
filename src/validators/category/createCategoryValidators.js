994fc0cfaf9ddbebf528f29f1aaf59d7b3c8b567
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-06-08 00:08:45 +0200

    Refactor validator create and update category

--- src/validators/category/createCategoryValidators.js
+++ src/validators/category/createCategoryValidators.js
@@ -1,17 +1,19 @@
 const { body, validationResult } = require('express-validator');
 const CategoryRepository = require('../../repositories/category/repository');
 
+const isCategoryWithNameExists = async (name) => {
+    const category = await CategoryRepository.getCategoryByName(name);
+    if (category) {
+        throw new Error('Category name already exists');
+    }
+}
+
 const createCategoryValidator = [
     body('name')
         .trim()
         .notEmpty().withMessage('Name is required')
         .isLength({ min: 3 }).withMessage('Name must be at least 3 characters long')
-        .custom(async (name) => {
-            const category = await CategoryRepository.getCategoryByName(name);
-            if (category) {
-                throw new Error('Category name already exists');
-            }
-        })
+        .custom(isCategoryWithNameExists)
 ];
 
 module.exports = { createCategoryValidator };
--- src/validators/category/updateCategoryValidators.js
+++ src/validators/category/updateCategoryValidators.js
@@ -1,18 +1,20 @@
 const { body, validationResult } = require('express-validator');
 const CategoryRepository = require("../../repositories/category/repository");
 
+const isCategoryNameExistsWithDifferentId = async (name, { req }) => {
+    const categoryId = req.params.id;
+    const category = await CategoryRepository.getCategoryByNameAndNotEqualId(name, categoryId);
+    if (category) {
+        throw new Error('Category name already exists');
+    }
+}
+
 const updateCategoryValidator = [
     body('name')
         .trim()
         .notEmpty().withMessage('Name is required.')
         .isLength({ min: 3 }).withMessage('Name must be at least 3 characters long')
-        .custom(async (name, { req }) => {
-            const categoryId = req.params.id;
-            const category = await CategoryRepository.getCategoryByNameAndNotEqualId(name, categoryId);
-            if (category) {
-                throw new Error('Category name already exists');
-            }
-        })
+        .custom(isCategoryNameExistsWithDifferentId)
 ];
 
 module.exports = { updateCategoryValidator };
