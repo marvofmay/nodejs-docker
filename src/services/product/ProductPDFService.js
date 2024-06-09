ce851a8721d9025fbef7d4c3dd4c1f5e7610b353
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-06-09 20:52:31 +0200

    Make pdf product service - photos

--- src/services/product/ProductPDFService.js
+++ src/services/product/ProductPDFService.js
@@ -22,6 +22,29 @@ class ProductPDFService {
             doc.fontSize(12).text('Is active:' + (product.active ? 'Yes' : 'No'), 100, 270);
             const categoriesTxt = product.categories.map(category => category.name).join(', ');
             doc.fontSize(12).text(`Categories: ${categoriesTxt}`, 100, 290);
+
+            let yPosition = 310;
+            const maxHeight = 200;
+            const maxWidth = 200;
+            const imageGap = 20;
+
+            product.photos.forEach(photo => {
+                if (photo.buffer) {
+                    const buffer = photo.buffer.buffer ? Buffer.from(photo.buffer.buffer) : photo.buffer;
+
+                    doc.image(buffer, 100, yPosition, {
+                        fit: [maxWidth, maxHeight],
+                        align: 'center',
+                        valign: 'center'
+                    });
+
+                    yPosition += maxHeight + imageGap;
+                    if (yPosition > doc.page.height - maxHeight) {
+                        doc.addPage();
+                        yPosition = 50;
+                    }
+                }
+            });
         });
 
         return outputPath;
