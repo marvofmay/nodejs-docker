df55ef282b9ce57687f6f849336585aec992ec04
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-06-12 00:29:08 +0200

    Remove unnecessary import

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const session = require('express-session');
