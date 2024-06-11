6d67250fc863471a5a9283629ed1500a4151bf1d
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-06-12 00:22:09 +0200

    Add custom validator for login and password

--- src/validators/login/loginValidators.js
+++ src/validators/login/loginValidators.js
@@ -1,15 +1,18 @@
-const { body, validationResult } = require('express-validator');;
-
+const { body } = require('express-validator');
 
 const loginValidator = [
     body('login')
         .trim()
-        .notEmpty().withMessage('login is required')
-        .isLength({ min: 5 }).withMessage('login must be at least 3 characters long'),
+        .notEmpty().withMessage('Login is required')
+        .isLength({ min: 5 }).withMessage('Login must be at least 5 characters long')
+        .matches(/^[a-zA-Z0-9]+$/).withMessage('Login can only contain letters and numbers, no spaces allowed'),
     body('password')
         .trim()
-        .notEmpty().withMessage('password is required')
-        .isLength({ min: 5 }).withMessage('password must be at least 3 characters long'),
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+        .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+        .matches(/\d/).withMessage('Password must contain at least one digit')
+        .matches(/[!@#&]/).withMessage('Password must contain at least one special character from !@#&')
 ];
 
 module.exports = { loginValidator };
