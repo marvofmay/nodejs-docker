82e0a34a09ec06639eecde55bcd0b97688fa340e
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-06-16 14:26:35 +0200

    Add swagger definition for getCstegoryById endpoint

--- src/controllers/api/v1/category/getCategoriesController.js
+++ src/controllers/api/v1/category/getCategoriesController.js
@@ -5,6 +5,7 @@ const categoryApiV1Repository = require('../../../../repositories/api/v1/categor
  * /categories:
  *   get:
  *     summary: Retrieve a list of categories
+ *     tags: [Category]
  *     description: Retrieve a list of categories with optional sorting and filtering by name.
  *     parameters:
  *       - in: query
--- src/controllers/api/v1/category/getCategoryByIdController.js
+++ src/controllers/api/v1/category/getCategoryByIdController.js
@@ -1,5 +1,43 @@
 const categoryApiV1Repository = require('../../../../repositories/api/v1/category/repository');
 
+/**
+ * @swagger
+ * /category/{id}:
+ *   get:
+ *     summary: Get category by ID
+ *     tags: [Category]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the category
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A category object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                 description:
+ *                   type: string
+ *                 createdAt:
+ *                   type: string
+ *                   format: date-time
+ *                 updatedAt:
+ *                   type: string
+ *                   format: date-time
+ *                 deletedAt:
+ *                   type: string
+ *                   format: date-time
+ *                   nullable: true
+ *       500:
+ *         description: Internal server error
+ */
 const getCategoryById = async (req, res) => {
     try {
        const data = await categoryApiV1Repository.getCategoryById(req);
