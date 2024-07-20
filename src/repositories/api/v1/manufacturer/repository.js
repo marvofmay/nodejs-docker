dc33b97734e4efa18946384bd828d02e63a64d7c
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-07-20 23:36:44 +0200

    Add param to query api for manufacturers

--- src/repositories/api/v1/manufacturer/repository.js
+++ src/repositories/api/v1/manufacturer/repository.js
@@ -1,18 +1,27 @@
 const Manufacturer = require("../../../../models/manufacturer");
+const Product = require("../../../../models/product");
 
 const getManufacturers = async (params) =>
 {
-    const { page = 1, limit = 10, sort = 'createdAt', order = 'asc', ...filters } = params;
+    const { page = 1, limit = 10, sort = 'createdAt', order = 'asc', parentManufacturer, ...filters } = params;
 
     const pageInt = parseInt(page);
     const limitInt = parseInt(limit);
     const sortOrder = order === 'asc' ? 1 : -1;
     const query = { deletedAt: null, ...filters };
 
+    const populateFields = [];
+
+    if (parentManufacturer === 'true') {
+        populateFields.push('parentManufacturer');
+    }
+
     const manufacturersQuery = Manufacturer.find(query)
         .sort({ [sort]: sortOrder })
         .skip((pageInt - 1) * limitInt)
         .limit(limitInt);
+
+    populateFields.forEach(field => manufacturersQuery.populate(field));
 
     const manufacturers = await manufacturersQuery;
     const total = await Manufacturer.countDocuments(query);
--- src/repositories/api/v1/manufacturer/transformer/manufacturerTransformer.js
+++ src/repositories/api/v1/manufacturer/transformer/manufacturerTransformer.js
@@ -1,15 +1,29 @@
 const DateUtility = require('../../../../../utility/DateUtility');
 
 const manufacturerTransformer = (manufacturer) => {
-    return data = {
+    const data = {
         id: manufacturer._id,
         name: manufacturer.name,
-        description: manufacturer.description,
-        active: manufacturer.active,
+        shortName: manufacturer.shortName,
+        nip: manufacturer.nip,
+        regon: manufacturer.regon,
+        email: manufacturer.email,
+        www: manufacturer.www,
         createdAt: DateUtility.formatDateYmdHis(manufacturer.createdAt),
         updatedAt: DateUtility.formatDateYmdHis(manufacturer.updatedAt),
         deletedAt: manufacturer.deletedAt ? DateUtility.formatDateYmdHis(manufacturer.deletedAt) : null,
     };
+
+    if (manufacturer.parentManufacturer) {
+        data.parentManufacturer = {
+            id: manufacturer.parentManufacturer._id,
+            name: manufacturer.parentManufacturer.name,
+            shortName: manufacturer.parentManufacturer.shortName,
+            nip: manufacturer.parentManufacturer.nip,
+        };
+    }
+
+    return data;
 };
 
 module.exports = manufacturerTransformer;
