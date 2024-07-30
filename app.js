94a2517ec97c825abd3d60bca4becaa1964e8b4d
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-07-30 20:05:21 +0200

    Install passport-jwt and jsonwebtoken

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const initializeApp = async () => {
             cookie: {
                 secure: false,
                 httpOnly: false,
-                maxAge: 1000 * 60 * 10 // session max age in milliseconds
+                maxAge: 1000 * 60 * 10
             }
         }));
 
--- src/config/passport.js
+++ src/config/passport.js
@@ -23,6 +23,19 @@ const initializePassport = (passport) => {
         }
     }));
 
+    passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+        try {
+            const user = await User.findById(jwt_payload.id);
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        } catch (err) {
+            return done(err, false);
+        }
+    }));
+
     passport.serializeUser((user, done) => {
         done(null, user._id);
     });
