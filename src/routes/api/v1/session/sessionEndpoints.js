186fb4d989a577307b0f23d2dc1946d8df97d64c
marcin <marcin.jaroszynski.gdansk@gmail.com>
2024-07-30 23:39:39 +0200

    Authenticate endpoint logout by JWT

--- src/routes/api/v1/session/sessionEndpoints.js
+++ src/routes/api/v1/session/sessionEndpoints.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const LoginController = require('../../../../controllers/api/v1/session/LoginController');
 const LogoutController = require('../../../../controllers/api/v1/session/LogoutController');
-const passport = require('passport');
+const { authenticateJwt } = require('../../../../middleware/auth');
 
 const router = express.Router();
 
 router.post('/login', LoginController.login);
-router.post('/logout', passport.authenticate('jwt', { session: false }), LogoutController.logout);
+router.post('/logout', authenticateJwt, LogoutController.logout);
 
 module.exports = router;
