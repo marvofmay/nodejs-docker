516ddfcf88c1323ffea98500238679f2551da37c
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2024-08-02 12:02:43 +0200

    Run environments: prod, dev, test by docker compose

--- src/controllers/basic/basicHomeController.js
+++ src/controllers/basic/basicHomeController.js
@@ -1,6 +1,9 @@
 const basicHome = async (req, res) => {
     try {
-        res.render('basic/home', {title: 'Home'});
+        res.render('basic/home', {
+            title: 'Home',
+            env: process.env.NODE_ENV || 'development',
+        });
     } catch(error) {
         res.render('error/error', {title: 'error', message: error.message});
     }
