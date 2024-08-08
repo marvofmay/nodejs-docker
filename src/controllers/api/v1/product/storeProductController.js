11bdcf6d4aa2d539fd587b55469149be41b5ce97
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2024-08-08 14:07:56 +0200

    Add endpoint for store product

--- src/controllers/api/v1/product/storeProductController.js
+++ src/controllers/api/v1/product/storeProductController.js
@@ -1,27 +1,58 @@
 const CreateProductDTO = require('../../../../dto/product/CreateProductDTO');
 const { validationResult } = require('express-validator');
 const ProductService = require('../../../../services/product/ProductService');
+const categoryRepository = require("../../../../repositories/category/repository");
+const manufacturerRepository = require("../../../../repositories/manufacturer/repository");
+const CreatePhotoDTO = require('../../../../dto/photo/CreatePhotoDTO');
+const PhotoService = require('../../../../services/photo/PhotoService');
 
 const storeProduct = async (req, res) => {
     try {
-        const productService = new ProductService();
         const errors = validationResult(req);
-        const createProductDTO = new CreateProductDTO(req.body);
+        const categoriesFromDB = await categoryRepository.getAllCategoriesForSelectOptions();
+        const categoriesForSelect = categoriesFromDB.map(category => ({
+            _id: category._id,
+            name: category.name
+        }));
+        const manufacturers = await manufacturerRepository.getAllManufacturersForSelectOptions();
+        const manufacturersForSelect = manufacturers.map(manufacturer => ({
+            _id: manufacturer._id,
+            name: manufacturer.name
+        }));
+        let createProductDTO = new CreateProductDTO(req.body);
+        const productService = new ProductService();
+        const photoService = new PhotoService();
 
-        if (! errors.isEmpty()) {
+        // Sprawdź błędy walidacji
+        if (!errors.isEmpty()) {
             return res.status(400).json({
-                errors: errors.array(),
-                message: 'Validation failed',
-                success: false,
+                errors: errors.array()
             });
         }
+
+        // Obsługuje przesyłanie plików
+        let photos = [];
+        if (req.files) {
+            for (const { originalname, encoding, mimetype, buffer, size } of req.files) {
+                const createPhotoDTO = new CreatePhotoDTO(originalname, encoding, mimetype, buffer, size);
+                const createPhotoResult = await photoService.createPhoto(createPhotoDTO);
+                photos.push(createPhotoResult.photoId);
+            }
+        }
+
+        // Ustaw DTO produktu z identyfikatorami zdjęć
+        createProductDTO = new CreateProductDTO(req.body, photos);
         const createResult = await productService.createProduct(createProductDTO);
 
-        return res.status(201).json(createResult);
+        // Zwróć odpowiedź JSON
+        return res.status(201).json({
+            message: 'Product created successfully',
+            product: createResult
+        });
+
     } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
+        return res.status(500).json({
+            message: error.message
         });
     }
 };
--- src/routes/api/v1/product/productEndpoints.js
+++ src/routes/api/v1/product/productEndpoints.js
@@ -5,12 +5,13 @@ const { authenticateJwt } = require("../../../../middleware/auth");
 const deleteProductController = require("../../../../controllers/api/v1/product/deleteProductController");
 const { createProductValidator } = require("../../../../validators/product/createProductValidators");
 const storeProductController = require("../../../../controllers/api/v1/product/storeProductController");
-
+const multer = require('multer');
+const upload = multer();
 const router = express.Router();
 
 router.get('/', getProductsController.getProducts);
 router.get('/:id', getProductByIdController.getProductById);
-router.post('/', authenticateJwt, createProductValidator, storeProductController.storeProduct);
+router.post('/', upload.array('photos'), authenticateJwt, createProductValidator, storeProductController.storeProduct);
 router.delete('/', authenticateJwt, deleteProductController.deleteProduct);
 
 module.exports = router;
