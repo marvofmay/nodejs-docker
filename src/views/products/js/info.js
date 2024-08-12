4305a00cf163fa3b8ffceabca44c00224f31aa34
Marcin Jaroszyński <marcin.jaroszynski.gdansk@gmail.com>
2024-08-12 23:18:49 +0200

    Change confirmation delete product in info view

--- src/views/products/js/info.js
+++ src/views/products/js/info.js
@@ -1,19 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const btnDeleteProduct = document.querySelectorAll('a.btn-delete-product');
-    btnDeleteProduct.forEach(btnDeleteProduct => {
+    const btnDeleteProducts = document.querySelectorAll('a.btn-delete-product');
+    btnDeleteProducts.forEach(btnDeleteProduct => {
+        const productId = btnDeleteProduct.dataset.productId;
+        const productName = btnDeleteProduct.dataset.productName;
+
         btnDeleteProduct.addEventListener('click', (e) => {
-            const shouldDelete = confirm('Czy na pewno chcesz usunąć tę kategorię?');
-            if (shouldDelete) {
-                const endpoint = `/products/${btnDeleteProduct.dataset.doc}`;
-                fetch(endpoint, {
-                    method: 'DELETE',
-                })
-                    .then(response => response.json())
-                    .then(data => window.location.href = data.redirect)
-                    .catch(err => console.log(err));
-            } else {
-                e.preventDefault();
-            }
+            Swal.fire({
+                title: `Do you want to delete this product? \n "${productName}"`,
+                showCancelButton: true,
+                confirmButtonText: "Delete",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    const endpoint = `/products/${productId}`;
+                    fetch(endpoint, {
+                        method: 'DELETE',
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            const element = document.querySelector('#info-product-content');
+                            if (element) {
+                                element.remove();
+                                Toastify({
+                                    text: data.actionResult.message,
+                                    duration: 3000,
+                                    newWindow: true,
+                                    gravity: 'top',
+                                    position: 'right',
+                                    style: {
+                                        background: "linear-gradient(to right top, #8ec021, #82be29, #77bd31, #6abb37, #5eb93e)",
+                                    }
+                                }).showToast();
+                            }
+                        })
+                        .catch(err => console.log(err));
+                } else if (result.isDenied) {
+                    e.preventDefault();
+                }
+            });
         });
     });
 });
